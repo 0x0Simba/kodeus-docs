4b4990f502cc45f2e870818200cf84cd70cf6b80
dev <dev@kodeus.xyz>
2026-04-08 01:01:44 +0530

    feat: kodeus brand theme (dark + green accent + logo)

--- lib/layout.shared.tsx
+++ lib/layout.shared.tsx
@@ -1,11 +1,10 @@
 import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared';
-import { appName, gitConfig } from './shared';
+import { gitConfig } from './shared';
 
 export function baseOptions(): BaseLayoutProps {
   return {
     nav: {
-      // JSX supported
-      title: appName,
+      title: <img src="/logo.svg" alt="Kodeus" className="h-6" />,
     },
     githubUrl: `https://github.com/${gitConfig.user}/${gitConfig.repo}`,
   };
