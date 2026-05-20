58589298678b22cb690552f7aba6682edaae0b43
Rahul Yadav <rahul54784@gmail.com>
2026-05-20 10:45:52 +0530

    font added and sidebar width changed

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,9 @@
 import { RootProvider } from 'fumadocs-ui/provider/next';
 import './global.css';
-import { Inter } from 'next/font/google';
-
-const inter = Inter({
-  subsets: ['latin'],
-});
 
 export default function Layout({ children }: LayoutProps<'/'>) {
   return (
-    <html lang="en" className={`${inter.className} dark`} suppressHydrationWarning>
+    <html lang="en" className="dark" suppressHydrationWarning>
       <body className="flex flex-col min-h-screen">
         <RootProvider theme={{ defaultTheme: 'dark', forcedTheme: 'dark' }}>{children}</RootProvider>
       </body>
